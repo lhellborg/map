2c48765a3426a843e0c41bfe8924ffd3521df239
Linda Hellborg <linda.hellborg@gmail.com>
2016-03-16 16:21:40 +0100

    add default places

--- js/googleMap.js
+++ js/googleMap.js
@@ -13,10 +13,8 @@ function MapViewModel() {
   // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
   self.map;
-  self.type = "restaurant";
-  self.keyword = "Fischers Fritz";
   self.infowindow;
-  self.defaultPlaces = [
+  self.initPlaces = [
   {name: "Fischers Fritz", placeId: "ChIJN-CrHttRqEcRW8WVxZ0PUDk", type: "restaurant"},
   {name: "Lutter & Wegner", placeId: "ChIJn3aH2MtRqEcRFCq5E46U1Zs", type: "restaurant"},
   {name: "Maritim Hotel Berlin", placeId: "ChIJVW_VXbRRqEcROfZizUmpRGA", type: "hotel"},
@@ -47,46 +45,34 @@ function MapViewModel() {
       zoom: 14
     });
 
-    var infowindow = new google.maps.InfoWindow();
+    self.infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(self.map);
-    var requests = [
-    {placeId: 'ChIJN-CrHttRqEcRW8WVxZ0PUDk'},
-    {placeId: 'ChIJn3aH2MtRqEcRFCq5E46U1Zs'}
-    ];
 
-    for (var i = 0; i < self.defaultPlaces.length; i++) {
-      service.getDetails(requests[i], callback);
+    for (var i = 0; i < self.initPlaces.length; i++) {
+      service.getDetails(self.initPlaces[i], callback);
     }
+  } //end initMap
 
-    function callback(request, status) {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        createMarker(request);
-        console.log(request);
-      }
-    } //end callback
+  function callback(request, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      createMarker(request);
+      console.log(request);
+    }
+  } //end callback
 
-    function createMarker(place) {
-      var marker = new google.maps.Marker({
-        map: self.map,
-        position: place.geometry.location
-      });
+  function createMarker(place) {
+    var marker = new google.maps.Marker({
+      map: self.map,
+      position: place.geometry.location
+    });
 
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-          'Place ID: ' + place.place_id + '<br>' +
-          place.formatted_address + '</div>');
-        infowindow.open(self.map, this);
-      });
-    } //end createMarker
-
-
-    // google.maps.event.addListener(marker, 'click', function() {
-    //   infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-    //     'Place ID: ' + place.place_id + '<br>' +
-    //     place.formatted_address + '</div>');
-    //   infowindow.open(self.map, this);
-    // });
-
+    google.maps.event.addListener(marker, 'click', function() {
+      self.infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+        'Place ID: ' + place.place_id + '<br>' +
+        place.formatted_address + '</div>');
+      self.infowindow.open(self.map, this);
+    });
+  } //end createMarker
 
     // A Places Nearby search is initiated with a call to the PlacesService's nearbySearch() method,
     // which will return an array of PlaceResult objects.
@@ -100,33 +86,7 @@ function MapViewModel() {
     // };
     // service.nearbySearch(request, callback);
 
-  } //end initMap
 
-  // loop over the PlaceResult array from nearbySearch and calls the createMarker function
-  function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-        console.log(results[i]);
-      }
-    }
-  } //end callback
-
-  // puts a marker on all the places on the map defined by the callback function
-  function createMarker(place) {
-    var placeLoc = place.geometry.location;
-
-    var marker = new google.maps.Marker({
-      map: self.map,
-      position: place.geometry.location
-    });
-
-  // add eventListener to the markers for an infoWindow
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(place.name);
-      infowindow.open(self.map, this);
-    });
-  } //end createMarker
 } //end MapViewModel
 
 var model = new MapViewModel();
