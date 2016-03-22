f5be6d83ffeab59a1ec3baa0b30c1106225271b2
Linda Hellborg <linda.hellborg@gmail.com>
2016-03-22 11:19:22 +0100

    jsbeautifer

--- src/js/navbarHeader.js
+++ src/js/navbarHeader.js
@@ -1,38 +1,38 @@
 //clickable navbar header
 $('#mapButton').on('click', function() {
-	$('.change').each(function() {
-		$(this).removeClass('active'); //take away the active class on any change item
-	});
-	$('.mainWindow').each(function() {
-		$(this).addClass('hidden'); //add a hidden class on all mainWindows
-	});
-	$(this).addClass('active'); //add active class on the clicked item
-	$("#map").removeClass('hidden'); //remove hidden class on the element to be shown in hte main window
+    $('.change').each(function() {
+        $(this).removeClass('active'); //take away the active class on any change item
+    });
+    $('.mainWindow').each(function() {
+        $(this).addClass('hidden'); //add a hidden class on all mainWindows
+    });
+    $(this).addClass('active'); //add active class on the clicked item
+    $("#map").removeClass('hidden'); //remove hidden class on the element to be shown in hte main window
 });
 
 $('#weatherButton').on('click', function() {
-	$('.change').each(function() {
-		$(this).removeClass('active'); //take away the active class on any change item
-	});
-	$('.mainWindow').each(function() {
-		$(this).addClass('hidden'); //add a hidden class on all mainWindows
-	});
-	$(this).addClass('active'); //add active class on the clicked item
-	$("#showWeather").removeClass('hidden'); //remove hidden class on the element to be shown in hte main window
+    $('.change').each(function() {
+        $(this).removeClass('active'); //take away the active class on any change item
+    });
+    $('.mainWindow').each(function() {
+        $(this).addClass('hidden'); //add a hidden class on all mainWindows
+    });
+    $(this).addClass('active'); //add active class on the clicked item
+    $("#showWeather").removeClass('hidden'); //remove hidden class on the element to be shown in hte main window
 });
 
 //taken form tutorial How to Build a Yahoo! Weather "Hello World" Application in JavaScript by Chris Wood.
 function getWeather() {
-	var location = "berlin";
-	//API request
-	$.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json', function (data) {
-		if (data.query.results === null) {         // Check that a place was found (we'll just grab the first)
-			bootbox.alert("Location not found: " + location + "!"); //error message
-		} else {
-			$('#showWeather').html('<h2>' + data.query.results.channel.item.title + '</h2>' +
-				data.query.results.channel.item.description);
-		}
-	});
+    var location = "berlin";
+    //API request
+    $.get('https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + location + '")&format=json', function(data) {
+        if (data.query.results === null) { // Check that a place was found (we'll just grab the first)
+            bootbox.alert("Location not found: " + location + "!"); //error message
+        } else {
+            $('#showWeather').html('<h2>' + data.query.results.channel.item.title + '</h2>' +
+                data.query.results.channel.item.description);
+        }
+    });
 }
 
 getWeather();
--- src/js/shortkey.js
+++ src/js/shortkey.js
@@ -1,17 +1,16 @@
-
 //trigger an event after pressing a key:
 
 document.addEventListener('keyup', function(e) {
 
-	e = e || window.event; // for IE to cover IEs window event-object
-	if (e.altKey && e.which == 65) { //press 'ALT + a'
-		var thaiFood = "thai food";
-		model.specialPlace(thaiFood); //thai food will be searched for with specialPlace function
-		return false;
-	}
-	if (e.altKey && e.which == 83) { //press 'ALT + s'
-		var emergency = "emergency";
-		model.specialPlace(emergency); //emergency will be search for with specialPlace function
-		return false;
-	}
+    e = e || window.event; // for IE to cover IEs window event-object
+    if (e.altKey && e.which == 65) { //press 'ALT + a'
+        var thaiFood = "thai food";
+        model.specialPlace(thaiFood); //thai food will be searched for with specialPlace function
+        return false;
+    }
+    if (e.altKey && e.which == 83) { //press 'ALT + s'
+        var emergency = "emergency";
+        model.specialPlace(emergency); //emergency will be search for with specialPlace function
+        return false;
+    }
 });
