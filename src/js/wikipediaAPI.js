935a8079b91534f05e33686d5e9a558563d57d7b
Linda Hellborg <linda.hellborg@gmail.com>
2016-03-29 10:00:37 +0200

    add ajax.fail for wikipedia

--- src/js/wikipediaAPI.js
+++ src/js/wikipediaAPI.js
@@ -3,10 +3,6 @@ var wikiRequest = function(name) {
     var wikiURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" +
         name + "&prop=info&inprop=url&rvprop=content&format=json&callback=wikiCallback";
 
-    //error handling when running for too long
-    var wikiRequestTimeout = setTimeout(function() {
-        $(".wiki-links").text("Sorry, no wikipedia page could be found");
-    }, 2000);
 
     $.ajax({
         url: wikiURL,
@@ -17,12 +13,12 @@ var wikiRequest = function(name) {
                 var name = data[1][i];
                 var url = data[3][i];
                 $(".wiki-links").append("<li><a href=" + url + " target=_blank>" + name + "</a></li>"); //when clicked on a link a new tab opens
-                if (url !== null) {
-                    clearTimeout(wikiRequestTimeout); //timeOut of the error message is taken away
-                }
+
             }
-
-
         }
+
+    })
+    .fail(function() {
+         $(".wiki-links").text("Sorry, no wikipedia page could be found");
     });
 };
